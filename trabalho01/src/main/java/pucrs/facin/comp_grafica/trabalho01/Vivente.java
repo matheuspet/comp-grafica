724166e132103ca06a16278164f449ef080f404e
unknown <13104533@w3331287.portoalegre.pucrsnet.br>
2016-08-10 19:00:06 -0300

    Criei a classe Vivente.java onde representaremos uma pessoa, que possui uma lista de movimentos, que é um ponto 2D e uma unidade no tempo.

---
+++ trabalho01/src/main/java/pucrs/facin/comp_grafica/trabalho01/Vivente.java
@@ -0,0 +1,14 @@
+package pucrs.facin.comp_grafica.trabalho01;
+
+import java.awt.Point;
+import java.util.List;
+import java.util.Map;
+
+public class Vivente {
+	List<Map<Point,Integer>> movimentos;
+	
+	public Vivente(){
+		// TODO Instanciar e preencher a lista de movimentos
+		
+	}
+}
