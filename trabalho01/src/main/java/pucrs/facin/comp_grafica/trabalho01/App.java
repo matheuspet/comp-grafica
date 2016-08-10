49dad4c8c9b468c3fd1d85db548c72890fb795d8
unknown <13104533@w3331287.portoalegre.pucrsnet.br>
2016-08-10 18:51:12 -0300

    Projeto do maven criado no Eclipse.

---
+++ trabalho01/src/main/java/pucrs/facin/comp_grafica/trabalho01/App.java
@@ -0,0 +1,11 @@
+package pucrs.facin.comp_grafica.trabalho01;
+
+/**
+ * @author Matheus Mattos e Thomás Vieira
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
